7c01a15e1aca0d5ca0c29a4114692651013ca83e
sergeimitrochenko <aron.armenyan@yandex.ru>
2025-01-18 19:25:48 +0300

    Начало проекта:задача №1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Задача № 1");
+        System.out.println("Имя сотрудника:");
+        String firstName;
+        firstName = " Ivan";
+        System.out.println(firstName);
+        System.out.println("Отчество сострудника:");
+        String middleName;
+        middleName = " Ivanovich";
+        System.out.println(middleName);
+        System.out.println("Фамилия сострудника:");
+        String lastName;
+        lastName = " Ivanov";
+        System.out.println(lastName);
+        String fullName = (lastName + firstName + middleName);
+        System.out.println("Ф.И.О.сотрудника:");
+        System.out.println(fullName);
+    }
+}
+
+
+
+
